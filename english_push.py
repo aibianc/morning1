e3c090e91e488b16c8f0264e408949e9567944aa
aibianc <98102108+aibianc@users.noreply.github.com>
2024-07-05 13:03:24 +0800

    Update english_push.py

--- english_push.py
+++ english_push.py
@@ -46,7 +46,7 @@ try:
         weiyu = None
         for item in weiyu_list:
             if item.startswith("【微语】"):
-               
+               weiyu = item[5:]
                 break
         
         if weiyu:
